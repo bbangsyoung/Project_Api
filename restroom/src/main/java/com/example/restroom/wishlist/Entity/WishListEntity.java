50937497a1c6e62723f4c69134945e347e5821ed
bbangdi <bbangord@gmail.com>
2022-11-22 04:06:15 +0900

    Entity 설정

---
+++ restroom/src/main/java/com/example/restroom/wishlist/Entity/WishListEntity.java
@@ -0,0 +1,24 @@
+package com.example.restroom.wishlist.Entity;
+
+import com.example.restroom.db.MemoryDbEntity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class WishListEntity extends MemoryDbEntity {
+
+    private String title;                   //장소
+    private String category;                //카테고리
+    private String address;                 //주소
+    private String readAddress;             //도로명
+    private String imageLink;               //이미지주소
+    private boolean isVisit;                //방문여부
+    private int visitCount;                 //방문횟수
+    private LocalDateTime lastVisitDate;    //마지막방문일
+
+}
