9f8df6a93903adbb82bd97f4c43808e8bc6659a2
bbangdi <bbangord@gmail.com>
2022-11-22 04:06:35 +0900

    repository 구현

---
+++ restroom/src/main/java/com/example/restroom/wishlist/repository/WishListRepository.java
@@ -0,0 +1,11 @@
+package com.example.restroom.wishlist.repository;
+
+import com.example.restroom.db.MemoryDbRepositoryAbstract;
+import com.example.restroom.wishlist.Entity.WishListEntity;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class WishListRepository extends MemoryDbRepositoryAbstract<WishListEntity> {
+
+
+}
