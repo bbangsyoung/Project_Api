431b6fd43a19980cd9f51c049a55a18c3c2a4ec1
bbangdi <bbangord@gmail.com>
2022-11-22 04:05:57 +0900

    main class

---
+++ restroom/src/main/java/com/example/restroom/RestroomApplication.java
@@ -0,0 +1,13 @@
+package com.example.restroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestroomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestroomApplication.class, args);
+	}
+
+}
