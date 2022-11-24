553bba53cd99dca6a1f71babacaba271c7b3fbb3
bbangdi <bbangord@gmail.com>
2022-11-24 18:50:55 +0900

    Service 구현

---
+++ restroom/src/main/java/com/example/restroom/wishlist/dto/WishListDto.java
@@ -0,0 +1,23 @@
+package com.example.restroom.wishlist.dto;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class WishListDto {
+
+    private int index;
+
+    private String title;                   //장소
+    private String category;                //카테고리
+    private String address;                 //주소
+    private String readAddress;             //도로명
+    private String imageLink;               //이미지주소
+    private boolean isVisit;                //방문여부
+    private int visitCount;                 //방문횟수
+    private LocalDateTime lastVisitDate;    //마지막방문일
+
+}
---
+++ restroom/src/main/java/com/example/restroom/wishlist/service/WishListService.java
@@ -0,0 +1,59 @@
+package com.example.restroom.wishlist.service;
+
+import com.example.restroom.naver.NaverClient;
+import com.example.restroom.naver.dto.SearchImageReq;
+import com.example.restroom.naver.dto.SearchLocalReq;
+import com.example.restroom.wishlist.dto.WishListDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class WishListService {
+
+    private final NaverClient naverClient;
+
+    public WishListDto search(String query) {
+        //지역검색
+        var searchLocalReq = new SearchLocalReq();
+        searchLocalReq.setQuery(query);
+
+        var SearchLocalRes = naverClient.searchLocal(searchLocalReq);
+
+        //데이터가 있을지 없을지 모르기 때문에 일단은 무조건 있다는 가정으로 if선언
+        if(SearchLocalRes.getTotal() > 0) {
+            var localItem = SearchLocalRes.getItems().stream().findFirst().get(); //무조건 존재
+
+            //띄어쓰기나 괄호 등이 있으면 모두 제외. 검색하기 용이하도록 필터링해주는 정규표현식
+            var imageQuery = localItem.getTitle().replaceAll("<[^>]*>","");
+
+
+
+            //**지역검색이 있을 때만 이미지검색!!!
+            var searchImageReq = new SearchImageReq();
+            searchImageReq.setQuery(imageQuery);
+
+            var searchImageRes = naverClient.searchImage(searchImageReq);
+
+            if(searchImageRes.getTotal() > 0) {
+                var imageItem = searchImageRes.getItems().stream().findFirst().get();
+                //결과리턴
+                var result = new WishListDto();
+                result.setTitle(localItem.getTitle());
+                result.setCategory((SearchLocalRes.getCategory()));
+                result.setAddress(localItem.getAddress());
+                result.setReadAddress(localItem.getRoadAdress());
+                result.setImageLink(imageItem.getLink());
+
+                return result;
+            }
+        }
+
+        return new WishListDto();
+
+
+    }
+
+
+
+}
