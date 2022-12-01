4f49ea06e46041bbbb2ba0f1a0006ce1c3414082
bbangdi <bbangord@gmail.com>
2022-12-01 23:17:58 +0900

    타입변경

--- restroom/src/main/java/com/example/restroom/wishlist/Entity/WishListEntity.java
+++ restroom/src/main/java/com/example/restroom/wishlist/Entity/WishListEntity.java
@@ -15,7 +15,7 @@ public class WishListEntity extends MemoryDbEntity {
     private String title;                   //장소
     private String category;                //카테고리
     private String address;                 //주소
-    private String readAddress;             //도로명
+    private String roadAddress;             //도로명
     private String imageLink;               //이미지주소
     private boolean isVisit;                //방문여부
     private int visitCount;                 //방문횟수
