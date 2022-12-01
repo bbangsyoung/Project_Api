eab44f4fee9efbddeff7479878e8b81195439583
bbangdi <bbangord@gmail.com>
2022-12-01 23:18:27 +0900

    전체리스트 출력

--- restroom/src/main/java/com/example/restroom/RestroomApplication.java
+++ restroom/src/main/java/com/example/restroom/RestroomApplication.java
@@ -1,7 +1,10 @@
 package com.example.restroom;
 
+import com.example.restroom.wishlist.dto.WishListDto;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @SpringBootApplication
 public class RestroomApplication {
@@ -9,5 +12,7 @@ public class RestroomApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RestroomApplication.class, args);
 	}
+
+
 
 }
--- restroom/src/main/java/com/example/restroom/controller/Apicontroller.java
+++ restroom/src/main/java/com/example/restroom/controller/Apicontroller.java
@@ -3,11 +3,12 @@ package com.example.restroom.controller;
 import com.example.restroom.wishlist.dto.WishListDto;
 import com.example.restroom.wishlist.service.WishListService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+@Slf4j
 @RestController
 @RequestMapping("/api/restroom")
 @RequiredArgsConstructor
@@ -20,7 +21,20 @@ public class Apicontroller {
     public WishListDto search(@RequestParam String query) {
         return wishListService.search(query);
 
+    }
 
+    @PostMapping("")
+    public WishListDto add(@RequestBody WishListDto wishListDto) {
+        log.info("{}", wishListDto);
+
+        return wishListService.add(wishListDto);
+
+    }
+
+    //전체리스트 출력
+    @GetMapping("/all")
+    public List<WishListDto> findAll() {
+        return wishListService.findAll();
 
     }
 }
